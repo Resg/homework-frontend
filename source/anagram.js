ecf4698fa8b4eb88c1a73367334ef99733aa824f
Resg <sergey@panichev.su>
2019-09-23 18:39:05 +0300

    исправление

--- source/anagram.js
+++ source/anagram.js
@@ -1,15 +1,12 @@
-
-
 const anagram = (arrayWords) => {
     const anagramMap = arrayWords.reduce((result, word) =>
-        ({
-            ...result, [[...word.toLowerCase()].sort().join("")]:
-                result.hasOwnProperty([...word.toLowerCase()].sort().join(""))?[...result[[[...word.toLowerCase()].sort().join("")]], word] : [word]
-    }), {});
-
-
-    const result = Object.values(anagramMap).reduce((result, value) => { return ((value.length > 1) ? [...result, value.sort()]: result) }, []);
-    console.log(result);
-
+        {
+            const sorted = [...word.toLowerCase()].sort().join("");
+            return {
+                ...result, [sorted]:
+                    result.hasOwnProperty(sorted) ? [...result[sorted], word] : [word]
+            }
+    }, {});
+    const result = Object.values(anagramMap).reduce((result, value) => { return (value.length > 1) ? [...result, value.sort()]: result }, []);
     return result.sort();
 };
